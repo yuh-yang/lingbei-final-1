f002611838f60499985974891aeec168fe2f9a70
StevenEzio <43330997+StevenEzio@users.noreply.github.com>
2018-12-07 16:12:34 +0800

    12/7 简易评论功能增加

--- Luojia_Image/settings.py
+++ Luojia_Image/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -40,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website',
     'likes',
+    'django.contrib.sites',
+    'django_comments',
 ]
-
+SITE_ID  =  1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -122,9 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media").replace('\\','/')
 
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend',
 #其他后端
 ]
+
+#ckeditor setup
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- Luojia_Image/urls.py
+++ Luojia_Image/urls.py
@@ -13,6 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+import notifications.urls
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
@@ -28,4 +33,5 @@ urlpatterns = [
     path('timeline/',  website_views.make_historyline, name="make_timeline",), #历史记录
     path('infoflow/', website_views.info_flow, name="InfoFlow",), #信息流
     path('likes/', include('likes.urls')),
+    url(r'^comments/', include('django_comments.urls')),
 ]
